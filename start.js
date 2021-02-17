de2077be46b76cc1a71fbd201b6dd9e48011c3ad
u9g <43508353+u9g@users.noreply.github.com>
2021-02-17 16:41:14 -0500

    Update start.js

--- start.js
+++ start.js
@@ -22,7 +22,7 @@ async function start () {
   await fs.writeFile('gradle.properties', text)
   console.info('Config file written.')
   const child = require('child_process').exec('gradle run')
-  // child.stdout.pipe(process.stdout) // if you want the output of gradle
+  child.stdout.pipe(process.stdout) // if you want the output of gradle
   await once(child, 'exit')
   console.info('Gradle done.')
 }
